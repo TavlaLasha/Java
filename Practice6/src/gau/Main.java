0543b210ad7ce9ffed28b397d8aafefe0b33303f
TavlaLasha <lashatavlalashvili@yahoo.com>
2021-04-09 15:53:33 +0400

    HomeWork 6

---
+++ Practice6/src/gau/Main.java
@@ -0,0 +1,30 @@
+package gau;
+
+import Works.A;
+import Works.B;
+import Works.C;
+
+public class Main {
+
+    public static void main(String[] args) {
+        A a = new A();
+//        a.creteFolder();
+//        a.m2();
+//        a.m5();
+//        a.m6();
+//        a.m7();
+//        a.m8();
+//        a.m9();
+//        a.m10(12);
+//        a.m11("1010");
+
+
+//        B b = new B();
+//        b.creteFolder();
+//        b.m1();
+
+//        C c = new C();
+//        c.creteFolder();
+//        c.m1();
+    }
+}
